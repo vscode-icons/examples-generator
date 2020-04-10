0f33c92bf3645458c9cceb5965a3b5230b3d05e9
Jimi (Dimitris) Charalampidis <jimikar@gmail.com>
2020-04-10 19:39:33 +0300

    Add guard clause

--- src/examples.ts
+++ src/examples.ts
@@ -4,25 +4,44 @@ import { findDirectorySync, findFileSync } from './utils';
 import { ExamplesGenerator } from './examplesGenerator';
 
 export function main(): void {
-  const logger = new Logger();
-  const pargs = new YargsParser(logger).parse();
+  try {
+    const logger = new Logger();
+    const pargs = new YargsParser(logger).parse();
 
-  // Locate 'vscode-icons' root directory
-  const rootDir = findDirectorySync('vscode-icons');
+    // Locate 'vscode-icons' root directory
+    const rootDir = findDirectorySync('vscode-icons');
 
-  // Find files and folders path
-  const baseRegex =
-    'src(?:(?:\\/|\\\\)[a-zA-Z0-9\\s_@-^!#$%&+={}\\[\\]]+)*(?:\\/|\\\\)';
-  const filesPath = findFileSync(
-    new RegExp(`${baseRegex}supportedExtensions\\.js`),
-    rootDir,
-  )[0];
-  const foldersPath = findFileSync(
-    new RegExp(`${baseRegex}supportedFolders\\.js`),
-    rootDir,
-  )[0];
-  const files = require(filesPath).extensions;
-  const folders = require(foldersPath).extensions;
+    if (!rootDir) {
+      throw Error(
+        `Directory 'vscode-icons' could not be found.\n` +
+          `Try cloning the 'vscode-icons' repository first, in the parent directory.`,
+      );
+    }
 
-  new ExamplesGenerator(pargs, files, folders, logger).generate();
+    // Find files and folders path
+    const baseRegex =
+      'src(?:(?:\\/|\\\\)[a-zA-Z0-9\\s_@-^!#$%&+={}\\[\\]]+)*(?:\\/|\\\\)';
+    const filesPath = findFileSync(
+      new RegExp(`${baseRegex}supportedExtensions\\.js`),
+      rootDir,
+    )[0];
+    const foldersPath = findFileSync(
+      new RegExp(`${baseRegex}supportedFolders\\.js`),
+      rootDir,
+    )[0];
+
+    if (!filesPath || !foldersPath) {
+      throw Error(
+        `Looks like 'vscode-icons' has not been build yet.\n` +
+          `Try performing a build of 'vscode-icons' first.`,
+      );
+    }
+
+    const files = require(filesPath).extensions;
+    const folders = require(foldersPath).extensions;
+
+    new ExamplesGenerator(pargs, files, folders, logger).generate();
+  } catch (error) {
+    console.error(error.message || error);
+  }
 }
